216f0157eb36b0308a34b72c5d1f8dd2584aa5ca
LordBedford <acater@gmail.com>
2020-10-15 17:05:10 -0400

    Map Maker now generates a grid of colored boxes based on the map returned to it by AStar.py

--- MapMaker.py
+++ MapMaker.py
@@ -12,19 +12,20 @@ class MapMaker:
         #self.window.protocol("WM_DELETE_WINDOW", on_closing)
         self.map = Astar.mapGen()
         self.labels = []
-        self.c = Canvas(self.window, width=250, height=200)
+        self.c = Canvas(self.window, width=1500, height=1500)
         self.c.pack()
-
-
-    def updatewindow(self):
-        self.window.update()
+        colors = ["blue", "red","pink","green"]
         for i in range(len(self.map)):
             boxes = []
             for j in range(len(self.map[i])):
-                boxes.append(self.c.create_rectangle(20 * i, 20 * j, 20 * (i + 1), 20 * (j + 1), fill="blue",
-                                                          outline='blue'))
+                boxes.append(self.c.create_rectangle(10 * i, 10 * j, 10 * (i + 1), 10 * (j + 1), fill=colors[int(self.map[i][j])],
+                                                     outline='black'))
                 self.c.pack()
             self.labels.append(boxes)
+
+    def updatewindow(self):
+        self.window.update()
+
 
 
 
