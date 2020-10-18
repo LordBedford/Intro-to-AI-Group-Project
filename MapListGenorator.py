bfeef7ca16bec2e74e435778f062fb31e944d725
Peter Marchese <31332889+Pmarch365@users.noreply.github.com>
2020-10-18 17:01:14 -0400

    Some quick changes to test some things out

--- MapListGenorator.py
+++ MapListGenorator.py
@@ -5,7 +5,8 @@ import numpy as np
 
 for i in range(10):
     temp = MapCreator.mapGen(120, 160)
+    tempMap = temp[0]
     filenumber = str(i)
     savefile = open("maps/map%s.txt" % filenumber, 'w')
-    np.savetxt(savefile, temp, fmt='%s')
+    np.savetxt(savefile, tempMap, fmt='%s')
     print("Map", (i+1), ": completed")
--- driver.py
+++ driver.py
@@ -1,10 +1,18 @@
 from MapMaker import *
 from astarSearch import *
+from MapCreator import *
 #Drive class
 
-run = True
+#run = True
 
-map = MapMaker()
-while(run):
-   MapMaker.tick(map)
+#map = MapMaker()
+#while(run):
+#   MapMaker.tick(map)
 
+
+
+mapTuple = MapCreator.mapGen(120,160)
+map = mapTuple[0]
+start = (0,0)
+goal = (100,100)
+print(a_star(map, start, goal))
