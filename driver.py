4e038c25a291ce45b007d5478955ed926762ee3f
Peter Marchese <31332889+Pmarch365@users.noreply.github.com>
2020-10-18 17:21:31 -0400

    minor changes

--- driver.py
+++ driver.py
@@ -3,16 +3,16 @@ from astarSearch import *
 from MapCreator import *
 #Drive class
 
-#run = True
+run = True
 
-#map = MapMaker()
-#while(run):
-#   MapMaker.tick(map)
+map = MapMaker()
+while(run):
+   MapMaker.tick(map)
 
 
 
-mapTuple = MapCreator.mapGen(120,160)
-map = mapTuple[0]
-start = (0,0)
-goal = (100,100)
-print(a_star(map, start, goal))
+#mapTuple = MapCreator.mapGen(120,160)
+#map = mapTuple[0]
+#start = (0,0)
+#goal = (100,100)
+#print(a_star(map, start, goal))
